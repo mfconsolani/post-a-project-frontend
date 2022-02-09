3cce62fd95439a6f111868cf2220b426ffb1d472
Mauro Consolani <mfconsolani@gmail.com>
2022-02-09 21:48:25 +0100

    Start working on SignUp component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { ProjectCard } from './Components'
 import { fakeData } from './fakeData'
+import { Field, Form } from 'react-final-form'
+import axios from 'axios'
 
 //TODO
 //Header
@@ -10,14 +12,47 @@ import { fakeData } from './fakeData'
 //Post a project form
 //
 
+const onSubmit = async values => {
+  console.log(values)
+  return await axios.post('http://localhost:8080/api/auth/local/signup', values).then(res => console.log(res)).catch(err => console.log(err))
+}
+// http://localhost:8080/api/projects
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
+
+      
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <div>Sign Up Form</div>
+              <label>Email</label>
+              <Field
+                name="email"
+                component="input"
+                type="email"
+                placeholder="email@youremail.com"
+              />
+              <label>Password</label>
+              <Field
+                name="password"
+                component="input"
+                type="password"
+              />
+            </div>
+            <button>Submit</button>
+          </form>
+        )}
+      />
+      <div>hola</div>
       {
         fakeData.map(element => {
           return (
-            <ProjectCard {...element} key={element.id}/>
+            <ProjectCard {...element} key={element.id} />
           )
         })
       }
@@ -26,3 +61,9 @@ function App() {
 }
 
 export default App;
+
+// <Header>
+//   <SignIn/>
+//   <SignUp/>
+//   <Profile/>
+// </Header>
