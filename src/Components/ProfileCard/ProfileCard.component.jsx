644a9da93e4aa47b4daa74dcb3a41edfb0f167c3
Mauro Consolani <mfconsolani@gmail.com>
2022-04-18 23:31:51 +0200

    Continue customizing ProfileCard component

--- src/Components/ProfileCard/ProfileCard.component.jsx
+++ src/Components/ProfileCard/ProfileCard.component.jsx
@@ -1,4 +1,4 @@
-import { Paragraph, Pane } from "evergreen-ui";
+import { Paragraph, Pane, Avatar, Text, Badge } from "evergreen-ui";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import capitalizeFirstLetter from '../../helpers/capitalizeFirstLetter'
@@ -10,9 +10,10 @@ const ProfileCard = () => {
 
     useEffect(() => {
         const fetchUserProfiles = async () => {
-            return await axios.get('http://localhost:8080/api/users')
+            return await axios.get('http://localhost:8080/api/users/candidates/extended')
                 .then(res => {
                     console.log(res.data.payload)
+
                     setUserProfiles(res.data.payload)
                 })
                 .catch(err => console.log(err))
@@ -26,8 +27,8 @@ const ProfileCard = () => {
 
         userProfiles && userProfiles.map(elem => {
             return (
-                <div>
-                    <div className="card-container">
+                <Pane display="flex" flexDirection="column" alignItems="center" >
+                    <div className="card-container" key={elem.id + "d" + (elem.profile && elem.profile.id) + 3}>
                         <span className="pro">PRO</span>
                         <img className="round" src={`https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * 100)}.jpg`} alt="user" />
                         <h3>{elem.username ? capitalizeFirstLetter(elem.username) : elem.email.split('@')[0]}</h3>
@@ -55,23 +56,55 @@ const ProfileCard = () => {
                         </div>
                     </div>
 
-                <Pane elevation={4} float="left" display="flex" flexDirection="column">
-                    <Pane> 
-                        <Paragraph>
-                            Card Body
-                        </Paragraph>
-                    </Pane>
-                    <Pane>
-                        <Paragraph>
-                            Card Footer
-                        </Paragraph>
+                    <Pane
+                        key={elem.id + "d" + (elem.profile && elem.profile.id)}
+                        elevation={3}
+                        float="left"
+                        display="flex"
+                        flexDirection="column"
+                        borderRadius="5px"
+                        marginY={30}
+                        padding={20}
+                        width={300}>
+                        <Pane>
+                            <Pane display="flex" flexDirection="column" alignItems="center">
+                                <Paragraph>
+                                    Card Header
+                                </Paragraph>
+                                <Avatar
+                                    src={`https://randomuser.me/api/portraits/women/${Math.floor(Math.random() * 100)}.jpg`}
+                                    name="Alan Turing"
+                                    size={100}
+
+                                />
+                                <Text size={300}>{elem.username ? capitalizeFirstLetter(elem.username) : elem.email.split('@')[0]}</Text>
+                            </Pane>
+                            <Pane display="flex" flexDirection="column" alignItems="center">
+                                <Paragraph>
+                                    Location 🇦🇹
+                                </Paragraph>
+                                <Paragraph>
+                                    Role 🖥
+                                </Paragraph>
+                            </Pane>
+                        </Pane>
+                        <Pane>
+                            <Paragraph>
+                                Skills
+                            </Paragraph>
+                            <Pane>
+                                {elem.profile && elem.profile.skills.map(elem => {
+                                    return (
+                                        <Badge color="teal" marginRight={8}>
+                                            {elem.skill}
+                                        </Badge>)
+                                })}
+
+                            </Pane>
+                        </Pane>
                     </Pane>
                 </Pane>
 
-
-
-                </div>
-                
             )
         })
     )
